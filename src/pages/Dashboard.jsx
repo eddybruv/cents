83c775b0f61546a8ece034205a9f874bb94a3a8c
Edwin Ajong <edwinajong@gmail.com>
2025-10-13 20:59:48 -0500

    feat: integrate Plaid API for link token creation and public token exchange

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useCallback } from "react";
 import { usePlaidLink } from "react-plaid-link";
 import BaseLayout from "../layout/BaseLayout";
 import ActionBar from "../components/ActionBar";
@@ -9,30 +9,74 @@ import BudgetHeader from "../components/dashboard/BudgetHeader";
 import CategoryBudgets from "../components/dashboard/CategoryBudgets";
 import RecentTransactions from "../components/dashboard/RecentTransactions";
 import API from "../api/API";
+import { useAuth } from "../hooks/useAuthContext";
 
 const Dashboard = () => {
-  const [linkToken, setLinkToken] = useState(null);
+  const { plaidData, setPlaidData, user } = useAuth();
+
+  const exchangePublicToken = useCallback(
+    async (public_token) => {
+      try {
+        const { data } = await API.post("/api/plaid/exchange-public-token", {
+          public_token,
+          user_id: user.id,
+        });
+        const { access_token, item_id } = data;
+        setPlaidData((prev) => ({
+          ...prev,
+          accessToken: access_token,
+          itemId: item_id,
+        }));
+      } catch (error) {
+        console.error("Error exchanging public token:", error);
+      }
+    },
+    [setPlaidData, user],
+  );
 
   useEffect(() => {
     const fetchLinkToken = async () => {
       try {
-        const { link_token: token } = await API.post(
-          "/api/plaid/create-link-token",
-          { userId: "user-id" },
-        );
-
-        setLinkToken(token);
+        const { data } = await API.post("/api/plaid/create-link-token", {
+          userId: user.id,
+        });
+        const { link_token: token } = data;
+        setPlaidData((prev) => ({ ...prev, linkToken: token }));
       } catch (error) {
         console.error("Error fetching link token:", error);
       }
     };
 
-    if (!linkToken) {
+    if (!plaidData?.linkToken) {
       fetchLinkToken();
     }
-  }, [linkToken]);
+  }, [plaidData, setPlaidData, user]);
 
-  
+  useEffect(() => {
+    if (plaidData?.public_token && !plaidData?.accessToken) {
+      exchangePublicToken(plaidData.public_token, user.id).then(() => {});
+    }
+  }, [plaidData, exchangePublicToken, setPlaidData, user]);
+
+  const { open, ready } = usePlaidLink({
+    token: plaidData?.linkToken,
+    onSuccess: (public_token) => {
+      setPlaidData((prev) => ({ ...prev, public_token }));
+
+      exchangePublicToken(public_token, user.id);
+    },
+    onExit: (err, metadata) => {
+      // user exited the Link flow
+      console.log("err", err);
+      console.log("metadata", metadata);
+    },
+  });
+
+  useEffect(() => {
+    if (!plaidData?.public_token && ready) {
+      open();
+    }
+  }, [open, ready, plaidData]);
 
   return (
     <BaseLayout>
